8df954c2038c3d0c9cc9f11619d13c481593c5d3
Varaprasad-dev93 <varaprasadyoyo@gmail.com>
2025-04-12 12:59:21 +0530

    logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import Recorder from "./components/Recorder";
 import { useCompute } from "./lib/useCompute";
 import { RefreshCcwDot } from "lucide-react";
@@ -8,7 +8,7 @@ export default function AudioApp() {
   const [audioBlob, setAudioBlob] = useState(null);
   const [userAudio, setUserAudio] = useState("en");
   const [transcription, setTranscription] = useState("hi");
-  const { translate ,isUserPlaying } = useCompute();
+  const { translate, isUserPlaying } = useCompute();
   const inputAudioRef = useRef();
 
   const [isReady, setIsReady] = useState(false);
@@ -21,15 +21,14 @@ export default function AudioApp() {
     return () => clearTimeout(timeout);
   }, []);
 
-
-  useEffect(()=>{
-    if(isUserPlaying){
+  useEffect(() => {
+    if (isUserPlaying) {
       if (inputAudioRef.current) {
         inputAudioRef.current.src = URL.createObjectURL(audioBlob);
         inputAudioRef.current.play();
       }
     }
-  },[isUserPlaying])
+  }, [isUserPlaying]);
   const swapLanguages = () => {
     setUserAudio((prev) => {
       setTranscription(prev);
@@ -53,7 +52,6 @@ export default function AudioApp() {
 
   // This function will be called once the audio is recorded
   const handleAudioRecorded = (audio) => {
-    
     setAudioBlob(audio);
 
     const reader = new FileReader();
@@ -68,56 +66,66 @@ export default function AudioApp() {
 
   return (
     <>
-      <div className=" bg-white/5 backdrop-blur-md w-full shadow-lg rounded-lg border border-white/20">
-        <h1 className=" self-center text-center text-2xl ">Translation App</h1>
-        <div className=" flex justify-between font-bold text-center w-full ">
-          {/* <h1>Languagec choosen</h1> */}
-          <div className="pl-2">
-            <label className="block font-semibold">User's Language:</label>
-            <select
-              className=" p-2 border rounded-md mt-1"
-              value={userAudio}
-              onChange={(e) => setUserAudio(e.target.value)}
-            >
-              {indianLanguages.map((lang) => (
-                <option key={lang.code} value={lang.code}>
-                  {lang.name}
-                </option>
-              ))}
-            </select>
-          </div>
+      <Suspense fallback={<Loader/>}>
+        <div className=" bg-white/5 backdrop-blur-md w-full shadow-lg rounded-lg border border-white/20">
+          <h1 className=" self-center text-center text-2xl ">
+            Translation App
+          </h1>
+          <div className=" flex justify-between font-bold text-center w-full ">
+            {/* <h1>Languagec choosen</h1> */}
+            <div className="pl-2">
+              <label className="block font-semibold">User's Language:</label>
+              <select
+                className=" p-2 border rounded-md mt-1"
+                value={userAudio}
+                onChange={(e) => setUserAudio(e.target.value)}
+              >
+                {indianLanguages.map((lang) => (
+                  <option key={lang.code} value={lang.code}>
+                    {lang.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-          <button onClick={swapLanguages}>
-            <RefreshCcwDot className="active:animate-spin" />
-          </button>
+            <button onClick={swapLanguages}>
+              <RefreshCcwDot className="active:animate-spin" />
+            </button>
 
-          {/* Dropdown for Translation */}
-          <div className="mb-2 pr-2">
-            <label className="block ">Translate To:</label>
-            <select
-              className=" p-2 border rounded-md mt-1"
-              value={transcription}
-              onChange={(e) => setTranscription(e.target.value)}
-            >
-              {indianLanguages.map((lang) => (
-                <option key={lang.code} value={lang.code}>
-                  {lang.name}
-                </option>
-              ))}
-            </select>
+            {/* Dropdown for Translation */}
+            <div className="mb-2 pr-2">
+              <label className="block ">Translate To:</label>
+              <select
+                className=" p-2 border rounded-md mt-1"
+                value={transcription}
+                onChange={(e) => setTranscription(e.target.value)}
+              >
+                {indianLanguages.map((lang) => (
+                  <option key={lang.code} value={lang.code}>
+                    {lang.name}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        {audioBlob && (
-          <div className="mt-4">
-            {/* <h3>Recorded Audio</h3> */}
-            <audio ref={inputAudioRef} controls className="hidden" />
-          </div>
-        )}
-        {isReady==false ?<><Loader/></>:<Convo />}
-        <Recorder onAudioRecorded={handleAudioRecorded} />
-      </div>
+        <div>
+          {audioBlob && (
+            <div className="mt-4">
+              {/* <h3>Recorded Audio</h3> */}
+              <audio ref={inputAudioRef} controls className="hidden" />
+            </div>
+          )}
+          {isReady == false ? (
+            <>
+              <Loader />
+            </>
+          ) : (
+            <Convo />
+          )}
+          <Recorder onAudioRecorded={handleAudioRecorded} />
+        </div>
+      </Suspense>
     </>
   );
 }
